de110c25ba015ef830f7f97d16d084995994361b
WuHuan <kof1234@sina.com>
2016-04-28 20:18:35 +0800

    Unity编辑器列表组件

---
+++ Assets/Editor/ListView/helper/GUIClipHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+public static class GUIClipHelper
+{
+    private static Func<Rect> VisibleRect;
+
+    public static void InitType()
+    {
+        var tyGUIClip = Type.GetType("UnityEngine.GUIClip,UnityEngine");
+        if (tyGUIClip != null)
+        {
+            var piVisibleRect = tyGUIClip.GetProperty("visibleRect", BindingFlags.Static | BindingFlags.Public);
+            if (piVisibleRect != null)
+                VisibleRect = (Func<Rect>)Delegate.CreateDelegate(typeof(Func<Rect>), piVisibleRect.GetGetMethod());
+        }
+    }
+
+    public static Rect visibleRect
+    {
+        get
+        {
+            InitType();
+            return VisibleRect();
+        }
+    }
+}
